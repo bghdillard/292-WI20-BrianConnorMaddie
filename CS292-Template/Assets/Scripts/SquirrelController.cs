af7e0633861eecbf833a5f8345f1d59df8f60bf9
Brian <bgdillard@knox.edu>
2020-02-06 15:02:59 -0600

    Added Animations to Squirrel
    Created the idle, move, and death animations for the squirrel, and edited the squirrel script to use them.

--- CS292-Template/Assets/Scripts/SquirrelController.cs
+++ CS292-Template/Assets/Scripts/SquirrelController.cs
@@ -7,49 +7,81 @@ public class SquirrelController : MonoBehaviour
 
     GameObject Controller;
     GameController GC;
+    Animator anim;
     int row;
     int col;
     int health;
+    bool dead;
+    Rigidbody2D body;
 
     // Start is called before the first frame update
     void Start()
     {
         Controller = GameObject.Find("GameController");
         GC = Controller.GetComponent<GameController>();
+        anim = GetComponent<Animator>();
         row = 3;
         col = 3;
         health = 3;
+        dead = false;
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        int newRow = row;
-        int newCol = col;
+        if (!dead)
+        {
+            int newRow = row;
+            int newCol = col;
+            bool moveL = false;
+            bool moveR = false;
+            bool moveU = false;
+            bool moveD = false;
 
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            newCol -= 1;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            newCol += 1;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            newRow += 1;
-        }
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            newRow -= 1;
-        }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                newCol -= 1;
+                moveL = true;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                newCol += 1;
+                moveR = true;
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                newRow += 1;
+                moveU = true;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                newRow -= 1;
+                moveD = true;
+            }
 
-        if(GC.GetTerrain(newRow, newCol) == 0 || GC.GetTerrain(newRow, newCol) == 2)
-        {
-            row = newRow;
-            col = newCol;
+            if (GC.GetTerrain(newRow, newCol) == 0 || GC.GetTerrain(newRow, newCol) == 2)
+            {
+                row = newRow;
+                col = newCol;
+                if (moveL)
+                {
+                    anim.SetTrigger("MoveLeft");
+                }
+                else if (moveR)
+                {
+                    anim.SetTrigger("MoveRight");
+                }
+                else if (moveU)
+                {
+                    anim.SetTrigger("MoveUp");
+                }
+                else if (moveD)
+                {
+                    anim.SetTrigger("MoveDown");
+                }
+            }
         }
-
         transform.position = new Vector3(col - GC.offset, row, 0);
     }
 
@@ -64,7 +96,8 @@ public class SquirrelController : MonoBehaviour
         print(health);
         if(health == 0)
         {
-            Destroy(gameObject);
+            anim.SetTrigger("IsDead");
+            dead = true;
         }
     }
 }
