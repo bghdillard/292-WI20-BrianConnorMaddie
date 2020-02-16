07f952c2a352953e679349214c6c6a19fd179f3a
Unknown <crweeks@knox.edu>
2020-02-16 00:00:23 -0600

    Added Pedestrians
    and started on frisbee player

---
+++ CS292-Template/Assets/Scripts/PedestrianController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianController : MonoBehaviour
+{
+    public float speed = 1;
+    public int direction = -1;
+
+    public GameObject parentObj;
+    public GameController parent;
+    public GameObject squirrel;
+    public List<Sprite> sprites;
+    public AudioSource audioSource;
+    public AudioClip clip;
+    bool passed;
+
+    void Start()
+    {
+        Sprite newSprite = rc(sprites);
+        gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        audioSource = gameObject.GetComponent<AudioSource>();
+        passed = false;
+    }
+
+    public void SetFlip()
+    {
+        if (direction == -1)
+        {
+            gameObject.transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
+            //transform.localScale.x *= 1;
+            //gameObject.transform.Rotate(0, 180, 0);;
+        }
+    }
+
+    void Update()
+    {
+        //transform.position += new Vector3(-1 * parent.landSpeed * Time.deltaTime, 0, 0);
+        transform.position += new Vector3(0, direction * speed * Time.deltaTime, 0);
+
+        if(transform.position.x < -1){
+            Destroy(gameObject);
+        }
+        if(transform.position.y < -4){
+            Destroy(gameObject);
+        }
+        if(transform.position.y > 12){
+            Destroy(gameObject);
+        }
+
+        
+        float dist = Vector3.Distance(squirrel.transform.position, transform.position);
+        if(passed == false && dist < 4){
+            passed = true;
+            audioSource.Play();
+        }
+    }
+
+    private Sprite rc(List<Sprite> choices){
+        return choices[Random.Range(0, choices.Count)];
+    }
+}
---
+++ CS292-Template/Assets/Scripts/PedestrianGenController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.ParticleSystem;
+
+public class PedestrianGenController : MonoBehaviour
+{
+    public int minCooldown = 140;
+    public int maxCooldown = 200;
+    public PedestrianController pedestrianPrefab;
+
+    public GameController parent;
+    public GameObject squirrel;
+    public int direction;
+    int cooldown;
+
+    void Start()
+    {
+        cooldown = Random.Range(0, minCooldown);
+        //parent = parentObj.GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //transform.position += new Vector3(-1 * parent.landSpeed * Time.deltaTime, 0, 0);
+        if(cooldown <= 0){
+            PedestrianController tc = Instantiate(pedestrianPrefab, transform.position, Quaternion.identity);
+            tc.transform.parent = parent.terrainObject.transform;
+            tc.parent = parent;
+            tc.direction = direction;
+            tc.squirrel = squirrel;
+            tc.SetFlip();
+
+            cooldown = Random.Range(minCooldown, maxCooldown);
+        }
+        cooldown -= 1;
+
+        if(transform.position.x < -1){
+            Destroy(gameObject);
+        }
+    }
+}
